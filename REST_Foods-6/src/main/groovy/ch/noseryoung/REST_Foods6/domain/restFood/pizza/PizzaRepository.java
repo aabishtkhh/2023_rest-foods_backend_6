b4395d4ed9d0598fcd08fcd56fae233a2e122dce
Henrikochh <113605484+Henrikochh@users.noreply.github.com>
2023-06-22 09:31:19 +0200

    Bug fixes Pizza

--- REST_Foods-6/src/main/groovy/ch/noseryoung/REST_Foods6/domain/restFood/pizza/PizzaRepository.java
+++ REST_Foods-6/src/main/groovy/ch/noseryoung/REST_Foods6/domain/restFood/pizza/PizzaRepository.java
@@ -2,6 +2,8 @@ package ch.noseryoung.REST_Foods6.domain.restFood.pizza;
 
 import ch.noseryoung.REST_Foods6.domain.restFood.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
 }
