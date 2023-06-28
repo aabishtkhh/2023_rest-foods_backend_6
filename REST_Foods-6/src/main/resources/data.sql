43f60a39e8a4c2668158cdf4afc09eb0b351de4e
Henrikochh <113605484+Henrikochh@users.noreply.github.com>
2023-06-28 15:14:26 +0200

    Bug fixing data.sql

--- REST_Foods-6/src/main/resources/data.sql
+++ REST_Foods-6/src/main/resources/data.sql
@@ -3,13 +3,12 @@ VALUES
     ('Margherita', 16, 'Tomato, Mozzarella, Oregano', false, 'main', true, 'https://www.bettybossi.ch/rdbimg/bb_itku120801_0243a/bb_itku120801_0243a_r02_v003_x0020.jpg', 20),
     ('Prosciutto', 19.5, 'Ham, Tomato, Mozzarella', false, 'main', true , 'https://www.gutekueche.ch/upload/rezept/12049/1600x900_pizza-prosciutto-e-fungi.jpg', 20),
     ('Salami', 19, 'Salami, Tomato, Mozzarella', true, 'main', true, 'https://www.daskochrezept.de/sites/daskochrezept.de/files/styles/full_width_tablet_4_3/public/2021-05/pizza_salami.jpg?h=21272edb&itok=ipXn2R5l', 20),
-    ('Funghi', 17.5, 'Tomato Sauce, Mozzarella, Fresh Champignons', false, 'main', true, 'https://media.istockphoto.com/id/1170797228/photo/pizza-with-mushrooms-on-dark-grey-concrete-table.jpg?s=612x612&w=0&k=20&c=fYRNQR_96SDtZlUMNscT1n38nSlKTVpk3DpwCSXakk0=', 20),
     ('Carbonara', 19, 'Cream, Pecorino Romano Cheese, Spring Onions, Eggs, Black Pepper', true, 'main', true , 'https://sushi24.ge/images/thumbs/0001700_carbonara-pizza-33-cm_510.jpeg', 20),
 
     ('Spagetti', 20, 'Tomato, Basil, Mozzarella', false, 'main', false, 'https://thekitchengirl.com/wp-content/uploads/Instant-Pot-Spaghetti-B_89.jpg', 7),
     ('Ravioli', 15, 'Cream, Parmesan Cheese, Shallot, Ham *Available without Ham*', true, 'main', false, 'https://anitalianinmykitchen.com/wp-content/uploads/2022/09/mushroom-rav-sq-1-of-1.jpg', 8),
-    ('Tortellini', 15, 'Cream, Vegetable Bouillon,  Ham *Available without Ham*', true, 'main', false, 'https://plantifullybasedblog.com/wp-content/uploads/2019/03/tortellini-image.png', 8),
-    ('Cappelletti', 15, 'Cream, Parmigiano Cheese, Chard, Thyme, Yellow Onion', true, 'main', false, 'https://images.squarespace-cdn.com/content/v1/5ea9a4e382294041c8e958f5/1611574637113-Q7P04WLU6Y9XV4NRMZXU/cappelletti.jpg', 6);
+    ('Tortellini', 15, 'Cream, Vegetable Bouillon,  Ham *Available without Ham*', false, 'main', false, 'https://plantifullybasedblog.com/wp-content/uploads/2019/03/tortellini-image.png', 8),
+    ('Cappelletti', 15, 'Cream, Parmigiano Cheese, Chard, Thyme, Yellow Onion', false, 'main', false, 'https://images.squarespace-cdn.com/content/v1/5ea9a4e382294041c8e958f5/1611574637113-Q7P04WLU6Y9XV4NRMZXU/cappelletti.jpg', 6);
 
 INSERT INTO drinks ("name", price, image_url, "size")
 VALUES ('Cola', 3.5,
